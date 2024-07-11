002b34d3fbfd1de11267959df6576ef104d6447b
fadli25 <fadlizakaria222@gmail.com>
2024-07-11 19:39:53 +0100

    fix padding

--- lib/styles/styles.ts
+++ lib/styles/styles.ts
@@ -1,4 +1,5 @@
-export const cardStyle = "px-5 py-3 rounded-md text-textColor bg-white text-sm";
+export const cardStyle =
+  "px-5 !py-3 rounded-md text-textColor bg-white text-sm";
 
 export const inputStyle =
-  "block flex-1 border-0 bg-transparent py-3 px-4 text-gray-900 placeholder:text-gray-400 outline-none text-xs sm:text-sm sm:leading-6 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600";
+  "block flex-1 border-0 bg-transparent !py-3 px-4 text-gray-900 placeholder:text-gray-400 outline-none text-xs sm:text-sm sm:leading-6 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600";
