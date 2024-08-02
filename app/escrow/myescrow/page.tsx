93d9a95a80bf3a223ea9f45ed47fb58f34295b39
fadli25 <fadlizakaria222@gmail.com>
2024-08-02 02:52:27 +0100

    fix font

--- app/escrow/myescrow/page.tsx
+++ app/escrow/myescrow/page.tsx
@@ -5,33 +5,39 @@ import CardContract from "@/components/CardContract";
 import { getFounderEscrow } from "@/lib/NexusProgram/escrow/utils.ts/getFounderEscrow";
 import { fakeData } from "@/lib/fakedata/Data";
 import { Stack } from "@mui/material";
-import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  useAnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
 import React, { useEffect, useState } from "react";
 
 export default function page() {
-
   const [escrows, setEscrows] = useState<any[]>();
 
-  const anchorWallet = useAnchorWallet()
-  const wallet = useWallet()
-  const { connection } = useConnection()
+  const anchorWallet = useAnchorWallet();
+  const wallet = useWallet();
+  const { connection } = useConnection();
 
   const getEscrow = async () => {
     try {
-      console.log("wow")
-      const escrow = await getFounderEscrow(connection, anchorWallet!, "confirmed");
-      setEscrows(escrow)
+      console.log("wow");
+      const escrow = await getFounderEscrow(
+        connection,
+        anchorWallet!,
+        "confirmed"
+      );
+      setEscrows(escrow);
       console.log(escrow);
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   useEffect(() => {
-    if (!anchorWallet) return
-    getEscrow()
-  }, [anchorWallet])
-
+    if (!anchorWallet) return;
+    getEscrow();
+  }, [anchorWallet]);
 
   return (
     <div>
@@ -42,14 +48,22 @@ export default function page() {
           alignItems="center"
           className="text-textColor text-xs"
         >
-          <div className="text-base">My Open contracts</div>
+          <div className="text-base font-myanmar">My Open contracts</div>
           <div>View past contracts</div>
         </Stack>
 
         <Stack spacing={2.8} mt={3}>
-          {escrows && escrows.map((el, i) => (
-            <CardContract key={i} contractName={el.contractName} amount={Number(el.amount)} deadline={Number(el.deadline)} escrow={el.pubkey.toBase58()} type={1} />
-          ))}
+          {escrows &&
+            escrows.map((el, i) => (
+              <CardContract
+                key={i}
+                contractName={el.contractName}
+                amount={Number(el.amount)}
+                deadline={Number(el.deadline)}
+                escrow={el.pubkey.toBase58()}
+                type={1}
+              />
+            ))}
         </Stack>
       </Card>
     </div>
