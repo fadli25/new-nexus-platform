512451b1a9856dffb60ce6839acca83fc30f048e
fadli25 <fadlizakaria222@gmail.com>
2024-08-02 03:25:39 +0100

    change font

--- components/CardAccordionAccept.tsx
+++ components/CardAccordionAccept.tsx
@@ -2,20 +2,27 @@ import CardAppAccept from "@/components/CardAppAccept";
 import { approvePayment } from "@/lib/NexusProgram/escrow/ApprovePayment";
 import { rejectFreelancer } from "@/lib/NexusProgram/escrow/rejectFreelancer";
 import { Stack } from "@mui/material";
-import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  useAnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
 import React from "react";
 import Card from "./Card";
 
-
-export default function CardAccordionAccept({ children, data, title, type, escrowInfo }: any) {
-
+export default function CardAccordionAccept({
+  children,
+  data,
+  title,
+  type,
+  escrowInfo,
+}: any) {
   const anchorWallet = useAnchorWallet();
   const wallet = useWallet();
   const { connection } = useConnection();
 
   const approveSubmit = async () => {
     try {
-
       console.log(escrowInfo);
       console.log(data);
       console.log(escrowInfo.escrow.toBase58());
@@ -27,22 +34,25 @@ export default function CardAccordionAccept({ children, data, title, type, escro
         wallet,
         escrowInfo.escrow,
         escrowInfo.freelancerInfo.address
-      )
-
+      );
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   const RejectSubmit = async () => {
     try {
-
-      const tx = await rejectFreelancer(anchorWallet, connection, wallet, escrowInfo.escrow, data[0].pubkey);
-
+      const tx = await rejectFreelancer(
+        anchorWallet,
+        connection,
+        wallet,
+        escrowInfo.escrow,
+        data[0].pubkey
+      );
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
     <div>
@@ -52,7 +62,7 @@ export default function CardAccordionAccept({ children, data, title, type, escro
           justifyContent="space-between"
           alignItems="center"
         >
-          <div className="text-base sm:text-[20px] text-[#696969] font-[500]">
+          <div className="text-base sm:text-lg font-myanmar text-[#696969] font-[500]">
             {title}
           </div>
           <Stack flexDirection="row" gap={1}>
@@ -63,7 +73,17 @@ export default function CardAccordionAccept({ children, data, title, type, escro
       <Card className="rounded-t-none min-h-24 w-[98%] mx-auto">
         <Stack spacing={2}>
           {data.map((el: any, i: number) => (
-            <CardAppAccept key={i} title={el.userName} role={el.role} type={type} reject={RejectSubmit} approve={approveSubmit} escrow={el.escrow} apply={el.pubkey} escrowInfo={escrowInfo} />
+            <CardAppAccept
+              key={i}
+              title={el.userName}
+              role={el.role}
+              type={type}
+              reject={RejectSubmit}
+              approve={approveSubmit}
+              escrow={el.escrow}
+              apply={el.pubkey}
+              escrowInfo={escrowInfo}
+            />
           ))}
         </Stack>
       </Card>
