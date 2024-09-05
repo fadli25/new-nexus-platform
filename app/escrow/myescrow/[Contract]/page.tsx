96eba109d98f4e82e073d99b70ad59e033a90c2d
kenedilichi1 <henryobeta@gmail.com>
2024-09-06 00:21:39 +0100

    add copy funtionality

--- app/escrow/myescrow/[Contract]/page.tsx
+++ app/escrow/myescrow/[Contract]/page.tsx
@@ -42,6 +42,7 @@ export default function page() {
   const [isDescriptionModalOpen, setIsDescriptionModalOpen] = useState(false);
   const [originalDescription, setOriginalDescription] = useState("");
   const [descriptionError, setDescriptionError] = useState("");
+  const [copied, setCopied] = useState(false);
 
   const anchorWallet = useAnchorWallet();
   const { connection } = useConnection();
@@ -195,6 +196,24 @@ export default function page() {
     setOpenDispute(true);
   }
 
+  const copyToClipboard = () => {
+    const pageUrl = window.location.href;
+
+    navigator.clipboard
+      .writeText(pageUrl)
+      .then(() => {
+        setCopied(true);
+
+        // Hide the popup after 2 seconds
+        setTimeout(() => {
+          setCopied(false);
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
+  };
+
   return (
     <div>
       <div className="max-w-6xl mx-auto pt-4">
@@ -237,9 +256,17 @@ export default function page() {
             </Stack>
           </Card>
 
-          <div className="bg-white rounded-xl p-5 h-full hidden sm:block">
+          <div
+            className="bg-white rounded-xl p-5 h-full hidden sm:block cursor-pointer"
+            onClick={copyToClipboard}
+          >
             <Image src={linksvg} alt="" className="w-[30px] py-[3px]" />
           </div>
+          {copied && (
+            <div className="absolute top-28 left-1/2 transform -translate-x-1/2 mt-2 px-4 py-2 bg-gray-300 text-white rounded">
+              Copied!
+            </div>
+          )}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-4">
           <Card className="col-span-1 md:col-span-3" width="lg">
